15ca4cac769acb341bd0697658e1b372721919a9
Priya Aswani <priya.aswani28139@gmail.com>
2022-03-31 23:01:20 +0400

    Update import of authorsTableData

--- front_end/src/layouts/nurses/index.js
+++ front_end/src/layouts/nurses/index.js
@@ -28,7 +28,7 @@ import Footer from "examples/Footer";
 import DataTable from "examples/Tables/DataTable";
 
 // Data
-import authorsTableData from "layouts/tables/data/authorsTableData";
+import authorsTableData from "layouts/nurses/data/authorsTableData";
 
 function Nurses() {
   const { columns, rows } = authorsTableData();
