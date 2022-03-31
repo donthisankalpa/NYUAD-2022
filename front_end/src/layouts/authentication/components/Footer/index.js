53ebb31785b26044098781715f116ec035eefdaa
Sashank Silwal <sashanksilwal@hotmail.com>
2022-03-31 22:24:48 +0400

    removed a few buttons

--- front_end/src/layouts/authentication/components/Footer/index.js
+++ front_end/src/layouts/authentication/components/Footer/index.js
@@ -19,17 +19,17 @@ import PropTypes from "prop-types";
 // @mui material components
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
-import Icon from "@mui/material/Icon";
+// import Icon from "@mui/material/Icon";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 
 // Material Dashboard 2 React base styles
-import typography from "assets/theme/base/typography";
+// import typography from "assets/theme/base/typography";
 
 function Footer({ light }) {
-  const { size } = typography;
+  // const { size } = typography;
 
   return (
     <MDBox position="absolute" width="100%" bottom={0} py={4}>
